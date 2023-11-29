e0e937c05106d13c581eb03a2d5d171d74b53bb0
anchayhua <ncnr_quique@hotmail.com>
2023-11-29 15:24:38 -0500

    Update backend.tf

--- ocean/backend.tf
+++ ocean/backend.tf
@@ -1,1 +1,13 @@
+provider "docker" {
+  # Configuración del proveedor Docker
+}
+
+resource "docker_container" "node_backend" {
+  name  = "node_backend"
+  image = "nombre_de_tu_imagen_node"
+  ports {
+    external = 3000
+    internal = 3000
+  }
+}
 
