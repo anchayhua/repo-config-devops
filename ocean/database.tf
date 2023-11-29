c3c72809af958d607a56bccd09a5c822de5d98aa
anchayhua <ncnr_quique@hotmail.com>
2023-11-29 15:25:00 -0500

    Update database.tf

--- ocean/database.tf
+++ ocean/database.tf
@@ -1,1 +1,13 @@
+provider "docker" {
+  # Configuración del proveedor Docker
+}
+
+resource "docker_container" "mongodb_database" {
+  name  = "mongodb_database"
+  image = "nombre_de_tu_imagen_mongodb"
+  ports {
+    external = 27017
+    internal = 27017
+  }
+}
 
