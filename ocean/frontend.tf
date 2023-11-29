011e2ab82f6c6f68f63993309edb6bb7efdef177
anchayhua <ncnr_quique@hotmail.com>
2023-11-29 15:24:20 -0500

    Update frontend.tf

--- ocean/frontend.tf
+++ ocean/frontend.tf
@@ -1,1 +1,13 @@
+provider "docker" {
+  # Configuración del proveedor Docker
+}
+
+resource "docker_container" "ionic_frontend" {
+  name  = "ionic_frontend"
+  image = "nombre_de_tu_imagen_ionic"
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
 
